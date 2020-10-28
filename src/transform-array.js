cabbc9e139fb5fc40dd28ab8557388d3eb5f275c
Ulyana Valodzina <67861703+Valodzina@users.noreply.github.com>
2020-10-28 21:12:50 +0300

    feat" some tasks

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
+module.exports = function transform( arr ) {
+  if(arr==undefined||arr===null||Object.prototype.toString.call( arr ) !== '[object Array]')
+  throw new Error('THROWN');
+
+  let a= new Array();
+  for(let i=0; i<arr.length; i++)
+  {
+    if(arr[i]==="--discard-next")
+    i++
+    else if(arr[i]==="--discard-prev"&& a.length!==0&&arr[i-2]!=="--discard-next")
+    a.pop();
+    else if(arr[i]==="--double-next" && i!==arr.length-1)
+    a.push(arr[i+1]);
+    else if(arr[i]==="--double-prev" && a.length!==0 && arr[i-2]!=="--discard-next")
+    a.push(arr[i-1]);
+    else if(arr[i]!=="--discard-prev"&&arr[i]!=="--double-next" &&arr[i]!=="--double-prev"&&arr[i]!=="--discard-next") a.push(arr[i]);
+
+  }
+  return a;
+ // throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,18 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
+
+function ff(m, d) {
+let nd=30;
+if (m===1||m===3||m===5||m===7||m===8||m===10||m===12)
+nd=31;
+else if (m===2) nd=28;
+
+if(d>nd)
+ff(m+1, d-nd);
+
+  return m;
+}
 module.exports = function getSeason( date ) {
 
   if (date === undefined)
   return  'Unable to determine the time of year!';
 
-   if ( typeof date !=Date)
+   if ( typeof date !="object" ||date===null)
+   return  'Unable to determine the time of year!';
+
+   if(Object.prototype.toString.call(date) !== '[object Date]')
    throw new Error('THROWN');
-   //return 0;
+ 
+ 
 
    let day = date.getDate();
    let month =date.getMonth();
-   if (month>=3&&month<6)
+   let rm= ff(month, day);
+  
+   
+   if (rm>=2&&rm<5)
    return "spring";
-  //throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+   if (rm>=5&&rm<8)
+   return "summer";
+   if (rm>=8&&rm<11)
+   return "autumn";
+
+   return "winter";
+
+
 };
