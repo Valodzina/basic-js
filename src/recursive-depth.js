3cbac98c43537cd9e24279e06326a4a9e74dc32c
Ulyana Valodzina <67861703+Valodzina@users.noreply.github.com>
2020-10-30 16:26:55 +0300

    feat: chain

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,12 @@
 const CustomError = require("../extensions/custom-error");
-
+var dep=0;
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
+  calculateDepth(arr) {
     throw new CustomError('Not implemented');
+    dep++;
+
+    return dep;
+   // throw new CustomError('Not implemented');
     // remove line with error and write your code here
   }
 };
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,74 @@
 const CustomError = require("../extensions/custom-error");
 
+function isInteger(num) {
+  return (num ^ 0) === num;
+}
+function splt(str) {
+  
+  let arr= new Array();
+  arr= str.split(" )~~");
+  arr.pop();
+
+for(let i=0; i<arr.length; i++)
+arr[i]= arr[i].slice(2, arr[i].length)
+  return arr;
+}
+
+
 const chainMaker = {
+  s :"",
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return splt(this.s).length;
+
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    
+    this.s+="( " +value+ " )~~";
+    return this;
+
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof position!=="number" || isInteger(position)!==true)
+    {
+      this.s="";
+    throw new Error('THROWN'); }
+
+    let arr= splt(this.s);
+
+    if (arr.length<position)
+    {
+      this.s="";
+    throw new Error('THROWN'); }
+
+    arr.splice(position-1,1);
+    this.s="";
+    for(let i=0; i<arr.length; i++)
+    {
+      this.addLink(arr[i]);
+    }
+
+////////////////////////
+    return this;
+
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+      let arr= splt(this.s);
+      this.s="";
+     for(let i=arr.length-1; i>=0; i--)
+     {
+       this.addLink(arr[i]);
+     }
+
+    return this;
+
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+ this.s=this.s.slice(0, this.s.length -2);
+ let str= this.s;
+ this.s="";
+    return str;
+
   }
 };
 
